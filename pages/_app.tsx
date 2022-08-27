10f4fe41f3e7904e63f06ab043fe14904e2ee2c8
minci <0xminci@gmail.com>
2022-08-27 19:04:39 -0400

    ADDED: rainbowkit integration with L16

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,69 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { Chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+const LuksoL16Chain: Chain = {
+  id: 2828,
+  name: "L16",
+  network: "lukso",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Lukso",
+    symbol: "LYXt",
+  },
+  rpcUrls: {
+    default: "https://rpc.l16.lukso.network",
+  },
+  blockExplorers: {
+    default: {
+      name: "Explorer",
+      url: "https://explorer.execution.l16.lukso.network",
+    },
+  },
+  testnet: true,
+};
+
+const { chains, provider } = configureChains(
+  [LuksoL16Chain],
+  [
+    alchemyProvider({
+      apiKey: process.env.NEXT_ALCHEMY_ID,
+    }),
+    publicProvider(),
+    jsonRpcProvider({
+      rpc: (chain) => {
+        if (chain.id !== LuksoL16Chain.id) return null;
+        return { http: chain.rpcUrls.default };
+      },
+    }),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My Lusko Dapp",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
